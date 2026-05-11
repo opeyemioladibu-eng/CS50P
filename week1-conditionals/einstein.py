2bf4da91e937420872b8a2bc9f5d27448f6dd3e7
AbdulQoyyum Oladibu <oladibuabdulqoyyum707@gmail.com>
2026-05-12 00:10:03 +0100

    Delete week1-conditionals directory

--- week1-conditionals/einstein.py
+++
@@ -1,6 +0,0 @@
-# E = mc^2,wherein 𝐸 represents energy (measured in Joules), 𝑚 represents mass (measured in kilograms), and 𝑐 represents the speed of light (measured approximately as 300000000 meters per second), per Albert Einstein et al. Essentially, the formula means that mass and energy are equivalent.
-
-c = 300000000
-m = int(input("Enter Mass[kg] :"))
-E = m * c * c
-print(E)
--- week1-conditionals/indoor.py
+++
@@ -1,3 +0,0 @@
-user_input = str(input("Enter desired input: "))
-lower_case = user_input.lower()
-print(lower_case)
